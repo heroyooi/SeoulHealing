bb3ddb4bca2327578e926ff402555f19487a73f5
heroyooi <heoryooi1018@gmail.com>
2024-05-27 15:52:56 +0900

    서울힐링요양병원 배포 - 모바일까지

---
+++ m/js/ui.js
@@ -0,0 +1,257 @@
+var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ? true : false;
+var runToast = false;
+
+$.urlParam = function(name){
+  var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+  if (results == null){
+    return null;
+  }
+  else {
+    return decodeURI(results[1]) || 0;
+  }
+}
+
+var GUI = window.GUI || (function(){
+  return {
+    init: function(){
+      GUI.baseUI($(document));
+
+      AOS.init({ // https://github.com/michalsnik/aos#1-initialize-aos
+        duration: 600,
+        once: true,
+      });
+      
+      if ($('.menu-wrap').length) {
+        GUI.gnbMenu();
+      }
+      if ($('.menu-swiper-wrap').length) {
+        GUI.lnbMenu();
+      }
+    },
+    baseUI: function($this){
+      var _ = $this;
+      var $win = $(window);
+      var $header = _.find('#header');
+      var tabUI = _.find('.tab-base');
+      var selectUI = _.find(".select-base");
+      var popupUI = _.find('.popup-wrap');
+      var csPopupUI = _.find('.cs-popup-wrap');
+
+      if (tabUI.length) {
+        tabUI.each(function(){
+          var target = $(this).find('li.on').find('a').attr('href');
+          $(target).show();
+        })
+        tabUI.find('a').on('click', function(e){
+          e.preventDefault();
+          var target = $(this).attr('href');
+          $(this).closest('li').addClass('on');
+          $(this).closest('li').siblings().removeClass('on');
+          $(target).show();
+          $(target).siblings().hide();
+        });
+      }
+      if (selectUI.length) {
+        selectUI.find('select').each(function(){
+          if ($(this).closest('.select-base').hasClass('disabled')) {
+            return true;
+          }
+          if ($(this).find('option:eq(0)').attr('value') === undefined) {
+            $(this).find('option:eq(0)').val(0);
+          }
+          if (parseInt($(this).val()) === 0) {
+            $(this).closest('.select-base').removeClass('active');
+          } else {
+            $(this).closest('.select-base').addClass('active');
+          }
+        });
+        selectUI.find('select').on("change",function(){
+          if ($(this).closest('.select-base').hasClass('disabled')) {
+            return true;
+          }
+          if (parseInt($(this).val()) === 0) {
+            $(this).closest('.select-base').removeClass('active');
+          } else {
+            $(this).closest('.select-base').addClass('active');
+          }
+          $(this).prev().html($(this).find("option:selected").text());
+        }).prev().html(function() {
+          return $(this).next().find("option:selected").text();
+        });
+      }
+
+      if (popupUI.length) {
+        var posY = null;
+        var magnificPopupConfiguration = function() {
+          return {
+            beforeOpen: function() {
+              posY = window.pageYOffset;
+              $('html').css('overflow', 'hidden');
+              $('body').css({'position': 'fixed', 'top': -posY});
+            },
+            resize: function() {
+              if ($('.fix-bottom').length) {
+                var $coWrap = $('.fix-bottom .popup-wrap .con-wrap');
+                if ($coWrap.hasClass('ws')) {
+                  $coWrap.children('div').css('max-height', $win.height() - 202 + 'px');
+                }
+                if ($coWrap.outerHeight() > $win.height() / 3 * 2.3) {
+                  $coWrap.addClass('ws');
+                  $coWrap.children('div').css('max-height', $win.height() - 202 + 'px');
+                }
+              }
+            },
+            open: function() {},
+            beforeClose: function() {
+              $('html').css('overflow', '');
+              $('body').css({'position': '', 'top': ''});
+              window.scrollTo(0, posY);
+            }
+          }
+        }
+  
+        //팝업 (공통) - jquery magnific 팝업
+        _.find('.btn-base.disabled, .all-view.disabled, .add-mylist.disabled').on('click', function (e) { // 비활성화 버튼
+          e.preventDefault();
+        });
+        _.find('.btn-popup-modal a').magnificPopup({
+          type: 'inline',
+          preloader: false,
+          modal: false
+        });
+        $(document).on('click', '.b-mfp-close', function (e) {
+          e.preventDefault();
+          $.magnificPopup.close();
+        });
+        
+        _.find('.btn-popup-anim-1:not(.disabled) a, a.btn-popup-anim-1:not(.disabled)').magnificPopup({
+          type: 'inline',
+          fixedContentPos: true,
+          fixedBgPos: true,
+          overflowY: 'auto',
+          closeBtnInside: true,
+          preloader: false,
+          midClick: true,
+          removalDelay: 300,
+          mainClass: 'mfp-zin'
+        });
+        _.find('.btn-popup-anim-2:not(.disabled) a').magnificPopup({
+          type: 'inline',
+          fixedContentPos: false,
+          fixedBgPos: true,
+          overflowY: 'auto',
+          closeBtnInside: true,
+          preloader: false,
+          midClick: true,
+          removalDelay: 300,
+          mainClass: 'mfp-slide-b'
+        });
+        _.find('.btn-popup-anim-3:not(.disabled) a, a.btn-popup-anim-3:not(.disabled)').magnificPopup({
+          type: 'inline',
+          fixedContentPos: true,
+          fixedBgPos: true,
+          overflowY: 'auto',
+          closeBtnInside: true,
+          preloader: false,
+          midClick: true,
+          removalDelay: 300,
+          mainClass: 'fade-slideup fix-bottom',
+          callbacks: magnificPopupConfiguration()
+        });
+
+		  $('.popup-wrap').each(function(){
+          if ($(this).data('width')) {
+            $(this).css('width', $(this).data('width'));
+          }
+        });
+      }
+
+      if (csPopupUI.length) {
+        csPopupUI.find('.mfp-close').on('click', function(){
+          var target = $(this).closest('.cs-fullpage').attr('id');
+          closeCSPopup();
+        });
+        $('.cs-popup-dimm').on('click', function(){
+          var target = $(this).next('.cs-popup-wrap').attr('id');
+          closeCSPopup();
+        });
+      }
+    },
+    gnbMenu: function(){
+      var $areaMenu = $('.menu-wrap');
+
+      $('#gnb > li > a').on('click', function(e){
+        e.preventDefault();
+        if (!$(this).closest('li').hasClass('on')) {
+          $(this).closest('li').addClass('on');
+          $(this).closest('li').siblings().removeClass('on');
+        } else {
+          $(this).closest('li').removeClass('on');
+        }
+      });
+
+      $areaMenu.find('.btn-close').on('click touchstart', function(e){
+        e.preventDefault();
+        closeGNB();
+      });
+
+      $(document).on('load scroll', function(){
+        if ($(this).scrollTop() == 0) {
+          $('#header').removeClass('active');
+        } else {
+          $('#header').addClass('active');
+        }
+      });
+    },
+    lnbMenu: function(){
+      var menuSwiper = new Swiper('.menu-swiper', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        observer: true,
+        observeParents: true,
+        observeSlideChildren: true,
+        // loop: true,
+        navigation: {
+          nextEl: '.menu-swiper-wrap .swiper-btn.next',
+          prevEl: '.menu-swiper-wrap .swiper-btn.prev',
+        },
+      });
+      var activeIndex = $('.menu-swiper-wrap .swiper-slide.on').index();
+      if (activeIndex) {
+        menuSwiper.slideTo(activeIndex - 1, 0)
+      }
+    }
+  }
+}());
+
+$(function(){
+  GUI.init();
+});
+
+// 이중 팝업을 열고 닫기 위한 함수
+var openCSPopup = function(id) {
+  $('.cs-popup-dimm#' + id + '-dimm, .cs-popup-wrap#' + id).fadeIn(200);
+};
+var closeCSPopup = function(id) {
+  if (id) {
+    $('.cs-popup-dimm#' + id + '-dimm, .cs-popup-wrap#' + id).fadeOut(200);
+  } else {
+    $('.cs-popup-dimm, .cs-popup-wrap').fadeOut(200);
+  }  
+}
+var rememberY = null;
+var openGNB = function() {
+  $('.menu-wrap').addClass('on');
+  $('.menu-dimm').fadeIn(150);
+  $('.m-section#menu-area').addClass('on');
+  rememberY = window.pageYOffset;
+  $('html').css('overflow', 'hidden');
+  $('body').css({'position': 'fixed', 'top': -rememberY});
+}
+var closeGNB = function(){
+  $('.menu-wrap').removeClass('on');
+  $('.menu-dimm').fadeOut(150);
+  $('html').css('overflow', '');
+  $('body').css({'position': '', 'top': ''});
+  window.scrollTo(0, rememberY);
+}
